c0ab09a524c62b5e4a8dc9314e5abe4848f447d8
Madis <madis1995@gmail.com>
2015-03-10 19:05:58 +0200

    no message

--- src/Nupp.java
+++ src/Nupp.java
@@ -1,4 +1,23 @@
 
 public class Nupp {
+    public String getNimi() {
+        return nimi;
+    }
 
+    String nimi;
+
+    public int getAsukoht() {
+        return asukoht;
+    }
+
+    public void setAsukoht(int asukoht) {
+        this.asukoht = asukoht;
+    }
+
+    int asukoht;
+
+    public Nupp(String nimi, int asukoht) {
+        this.nimi = nimi;
+        this.asukoht = asukoht;
+    }
 }
