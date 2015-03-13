fe2bdccd13ed956915cd3324fa36bce2584103e7
Madis <madis1995@gmail.com>
2015-03-13 14:55:44 +0200

    Täring fixed

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,10 +1,9 @@
 
 public class Peaklass {
     public static void main(String[] args) {
+        Täring y = new Täring();
         for (int i = 0; i < 10; i++) {
-            Täring y = new Täring();
-            System.out.println(y.täringuvise);
-            y = null; //kaotab objekti, vist pole vaja, aga hea teada hiljemaks
+            System.out.println(y.vise());
         }
     }
 }
