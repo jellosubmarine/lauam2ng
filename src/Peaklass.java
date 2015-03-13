76159efdb37fdf96e770304c404108fad6a61262
teenlauam2ngu <markusveinla@hotmail.com>
2015-03-13 15:11:58 +0200

    Uued väljad

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,6 +1,7 @@
 
 public class Peaklass {
     public static void main(String[] args) {
+        
         for (int i = 0; i < 10; i++) {
             Täring y = new Täring();
             System.out.println(y.täringuvise);
---
+++ src/Väli2edasi.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Markus on 13.03.2015.
+ */
+public class Väli2edasi {
+    Hetkmängija.asukoht = Hetkmängija.asukojt+2;
+}
